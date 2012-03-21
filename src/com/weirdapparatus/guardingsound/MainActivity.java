ae962c3c0cbb1b4ebaf6451668f64c7d71017da8
memphisvl <vg.infobox@gmail.com>
2012-03-22 00:34:29 +0200

    Fixed onDestroy

--- src/com/weirdapparatus/guardingsound/MainActivity.java
+++ src/com/weirdapparatus/guardingsound/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onDestroy() {
+        super.onDestroy();
         if (mediaPlayer != null) {
             mediaPlayer.stop();
             mediaPlayer.release();
